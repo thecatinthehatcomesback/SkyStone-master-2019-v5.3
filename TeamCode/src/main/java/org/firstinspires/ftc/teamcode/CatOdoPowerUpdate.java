b5212a875dcd3b84a89a7b15aa96ba5a56a31cf1
ZeSturmCoder <PanzerFowst@gmail.com>
2019-12-02 21:34:51 -0600

    Did some more pseudo code and most of the bones are ready for meat...

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CatOdoPowerUpdate.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CatOdoPowerUpdate.java
@@ -33,23 +33,30 @@ public class CatOdoPowerUpdate {
      * Thoughts:
      *
      *
-     * For ramp up:
+     * Plans:
+     * 1.  First check to see if within ramp down range (if so, use scale down, otherwise
+     * start with the jump)
+     * 2.  Begin ramping up power while checking the distance left
+     * 3.  If ever within the distance in which we need to ramp down, ramp down.  Otherwise
+     * keep performing Step 4.
+     * 4.  If currentPower is greater than maxPower, don't change currentPower.  Otherwise
+     * keep ramping up.
      *
-     * 1.  Needs a initial jump up (to say 0.2) to initially get over the static friction
+     * For ramp UP:
+     * 1.  Needs a initial jump up (to say 0.2 power) to initially get over the static friction
      * 2.  Will then ramp up to max speed every time it wakes from the sleep period based
      * on the max power divided by time period left to get up to max speed to (0.5 sec e.g.)
      * 3.  Store this as currentPower then compare to max power (don't change anything if
      * current power is higher than maxPower)
      *
-     *
-     * For ramp down:
-     *
+     * For ramp DOWN:
      * 1.  Start ramping down as soon as within the distance it takes to slow down (e.g.
      * seven inches?)
-     * 2.  Use the minPower
+     * 2.  Use the minPower so that robot never stalls out until distance is reached.
+     *
      *
      *
      * currentPower = currentPower * (deltaDistance * rate (which could be 1/7 or some
-     * other calculation?)
+     * other calculation?))
      */
 }
