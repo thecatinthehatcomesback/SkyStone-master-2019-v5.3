891265a472239971efb396224c70db3123b99727
Cat <catcomesback10273@gmail.com>
2020-02-29 18:13:13 -0600

    Second round of testing.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test_Autonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test_Autonomous.java
@@ -114,5 +114,6 @@ public class Test_Autonomous extends LinearOpMode
         allPoints.add(new CurvePoint(0, 0, 3.0, 1.0, Math.toRadians(0), 1.0));
 
         robot.driveOdo.translateDrive(allPoints, .7, 0, 3.0, 8);
+        robot.driveOdo.waitUntilDone();
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test_MotorTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test_MotorTeleOp.java
@@ -1,63 +1,194 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.hardware.rev.RevBlinkinLedDriver;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+import java.util.ArrayList;
+
 /**
- * TestMotorTeleOp.java
+ * Mec_Odo_AutonomousLevel6_Statey.java
  *
  *
- * A Linear opMode class that is used to test different things in a TeleOp setting.
+ * A Linear OpMode class to be an autonomous method for both Blue & Red alliance sides where we pick
+ * which side of the alliance bridge we start off at with gamepad1 as well as selecting alliance
+ * color and whether we park under the alliance bridge closer or further from the game field wall.
+ * Also will detect the position and deliver the skystone using machine vision and move the
+ * foundation.
+ *
+ * Mec_Odo_AutonomousLevel6_Statey is written to use machine vision and SkyStone delivery to our
+ * autonomous route with the help intake jaws that suck in a stone at any orientation using a
+ * "touch it-own it" approach.  A servo and two motors make up TC-73/Bucky's arm and stack stones as
+ * well as our team marker.
+
+ * This autonomous is used for our State Championship(February 7-8, 2020).
  *
  *
- * @author FTC Team #10273, The Cat in the Hat Comes Back
+ * @author FTC Team #10273, The Cat in the Hat Comes Back.
  */
-@Disabled
-@TeleOp(name="Motor Test TeleOp", group="CatTest TeleOp")
+@Autonomous(name="State Odo Autonomous", group="CatAuto")
 public class Test_MotorTeleOp extends LinearOpMode
 {
     /* Declare OpMode members. */
-    private ElapsedTime elapsedGameTime = new ElapsedTime();
-    private ElapsedTime elapsedTime = new ElapsedTime();
+    CatHW_Async robot  = new CatHW_Async();    // All the hardware classes init here.
+    private ElapsedTime delayTimer = new ElapsedTime();
+    private double timeDelay;
+    private boolean isRedAlliance = true;
+    private boolean isBuildZone = false;
+    private boolean isParkAtWall = false;
+    private boolean isFoundation = true;
 
-    /* Declare OpMode members. */
-    CatHW_Async robot = new CatHW_Async();  // Use our mecanum asynchronous hardware
-
-    /* Constructor */
-    public Test_MotorTeleOp() {}
-
+    private CatHW_Vision.skyStonePos skyStonePos = CatHW_Vision.skyStonePos.OUTSIDE;
 
     @Override
     public void runOpMode() throws InterruptedException {
-        // Initialize the hardware
-        robot.init(hardwareMap, this, false);
 
-        // Define and Initialize Extra Motors: //
-
-        // Set Extra Motor and Servo Modes: //
-
-        // Wait for the game to start (driver presses PLAY)
-        waitForStart();
+        /*
+        Initialize the setDrivePowers system variables.  The init() methods of our hardware class
+        does all the work:
+         */
+        robot.init(hardwareMap, this, true);
 
 
-        // Go! (Presses PLAY)
-        elapsedGameTime.reset();
-        elapsedTime.reset();
+        /*
+        Init Delay Option Select:
+         */
+        // After init is pushed but before Start we can change the delay using dpad up/down //
+        delayTimer.reset();
+        // Runs a loop to change certain settings while we wait to start
+        while (!opModeIsActive()) {
+            if (this.isStopRequested()) {
+                // Leave the loop if STOP is pressed
+                return;
+            }
 
-        // Run infinitely until the end of the match (driver presses STOP)
-        while (opModeIsActive()) {
+            robot.eyes.findSkyStone();
+
+            if (gamepad1.dpad_up && (delayTimer.seconds() > 0.8)) {
+                // Increases the amount of time we wait
+                timeDelay += 1;
+                delayTimer.reset();
+            }
+            if (gamepad1.dpad_down && (delayTimer.seconds() > 0.8)) {
+                // Decreases the amount of time we wait
+                if (timeDelay > 0) {
+                    // No such thing as negative time
+                    timeDelay -= 1;
+                }
+                delayTimer.reset();
+            }
+            if (((gamepad1.x) && delayTimer.seconds() > 0.8)) {
+                // Changes Alliance Sides
+                if (isRedAlliance) {
+                    isRedAlliance = false;
+                    robot.isRedAlliance = false;
+                } else {
+                    isRedAlliance = true;
+                    robot.isRedAlliance = true;
+                }
+                delayTimer.reset();
+            }
+            if (((gamepad1.y) && delayTimer.seconds() > 0.8)) {
+                // Changes Alliance Sides
+                if (isBuildZone) {
+                    isBuildZone = false;
+                } else {
+                    isBuildZone = true;
+                }
+                delayTimer.reset();
+            }
+            if (((gamepad1.dpad_left) && delayTimer.seconds() > 0.8)) {
+                // Changes Alliance Sides
+                if (isParkAtWall) {
+                    isParkAtWall = false;
+                } else {
+                    isParkAtWall = true;
+                }
+                delayTimer.reset();
+            }
+            if (((gamepad1.dpad_right) && delayTimer.seconds() > 0.8)) {
+                // Changes Alliance Sides
+                if (isFoundation) {
+                    isFoundation = false;
+                } else {
+                    isFoundation = true;
+                }
+                delayTimer.reset();
+            }
+
+            /**
+             * LED code:
+             */
+            if (robot.isRedAlliance) {
+                robot.lights.setDefaultColor(RevBlinkinLedDriver.BlinkinPattern.RAINBOW_LAVA_PALETTE);
+            } else {
+                robot.lights.setDefaultColor(RevBlinkinLedDriver.BlinkinPattern.RAINBOW_OCEAN_PALETTE);
+            }
+            /**
+             * Telemetry while waiting for PLAY:
+             */
+            telemetry.addData("Delay Timer: ", timeDelay);
+
+            skyStonePos = robot.eyes.giveSkyStonePos();
+            if (skyStonePos == CatHW_Vision.skyStonePos.OUTSIDE && !isRedAlliance){
+                skyStonePos = CatHW_Vision.skyStonePos.CENTER;
+            }else if (skyStonePos == CatHW_Vision.skyStonePos.CENTER && !isRedAlliance){
+                skyStonePos = CatHW_Vision.skyStonePos.OUTSIDE;
+            }
 
 
+            telemetry.addData("Label", skyStonePos);
 
-            //--------------------------------------------------------------------------------------
-            // Telemetry Data:
-            //--------------------------------------------------------------------------------------
+            telemetry.addData("left position", robot.eyes.lastLeft);
+            telemetry.addData("right position", robot.eyes.lastRight);
+            telemetry.addData("center position", (robot.eyes.lastRight+robot.eyes.lastLeft)/2);
+            telemetry.addData("confidence", robot.eyes.lastConfidence);
 
-            //telemetry.addData("Test Power:", "%.2f", testMotor.getPower());
-            //telemetry.addData("Test Encoder:", "%d", testMotor.getCurrentPosition());
+            if (isRedAlliance) {
+                telemetry.addData("Alliance: ", "Red");
+            } else {
+                telemetry.addData("Alliance: ", "Blue");
+            }
+
+            telemetry.addData("isBuildZone Side? ", isBuildZone);
+            telemetry.addData("isParkAtWall ", isParkAtWall);
+            telemetry.addData("isFoundation ", isFoundation);
             telemetry.update();
+
+            /**
+             * We don't need a "waitForStart()" since we've been running our own
+             * loop all this time so that we can make some changes.
+             */
         }
+        /**
+         * Runs after hit start:
+         * DO STUFF FOR the OPMODE!!!
+         */
+
+        /**
+         * Init the IMU after play so that it is not offset after
+         * remaining idle for a minute or two...
+         */
+        robot.driveClassic.IMU_Init();
+
+        // Time Delay:
+        robot.robotWait(timeDelay);
+
+        /* Go! */
+
+
+        ArrayList<CurvePoint> allPoints = new ArrayList<>();
+
+        allPoints.add(new CurvePoint(0, 0, 3.0, 1.0, Math.toRadians(0), 1.0));
+        allPoints.add(new CurvePoint(0, 96, 3.0, 1.0, Math.toRadians(0), 1.0));
+        allPoints.add(new CurvePoint(72, 96, 3.0, 1.0, Math.toRadians(0), 1.0));
+        allPoints.add(new CurvePoint(72, 0, 3.0, 1.0, Math.toRadians(0), 1.0));
+        allPoints.add(new CurvePoint(0, 0, 3.0, 1.0, Math.toRadians(0), 1.0));
+
+        robot.driveOdo.translateDrive(allPoints, .7, 0, 3.0, 8);
+        robot.driveOdo.waitUntilDone();
+
+        robot.driveOdo.updatesThread.stop();
     }
 }
